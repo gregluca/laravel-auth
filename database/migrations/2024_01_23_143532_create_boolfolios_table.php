7deeca8787344166f5b58ca5331e624b64512ee9
Luca Gregori <luca.gregori98@gmail.com>
2024-01-23 16:39:30 +0100

    Add: migration-seeder

--- database/migrations/2024_01_23_143532_create_boolfolios_table.php
+++ database/migrations/2024_01_23_143532_create_boolfolios_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('boolfolios', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->date('project_date');
+            $table->string('slug');
             $table->timestamps();
         });
     }
